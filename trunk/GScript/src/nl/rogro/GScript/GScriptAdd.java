caa1c2ba428d2538e3d0b7f50f6bbe33eb91ecaa
rogro82 <rogro82@c1b9c7e0-0303-11de-bcc6-d9108be23e13>
2009-02-26 22:36:59 +0000

    Added the feature to load .sh files from the sdcard.

--- trunk/GScript/src/nl/rogro/GScript/GScriptAdd.java
+++ trunk/GScript/src/nl/rogro/GScript/GScriptAdd.java
@@ -1,8 +1,14 @@
 package nl.rogro.GScript;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
@@ -14,10 +20,43 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class GScriptAdd extends Activity {
+
 	SQLiteDatabase mDatabase;
 	EditText EditTextName;
 	EditText EditTextScript;
 	CheckBox CheckboxSu;
+	protected static final int ACTIVITY_LOAD_FILE = 9999;
+	
+	//Activity load file
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode==ACTIVITY_LOAD_FILE && resultCode==Activity.RESULT_OK)
+		{
+			String loadFileName = "";
+			
+			try {
+
+			loadFileName = "/sdcard/" + data.getStringExtra("FileToLoad");
+			String Script = "";
+			
+  	        BufferedReader in = new BufferedReader(new FileReader(loadFileName));
+  	        String str;
+  	        
+  	        while ((str = in.readLine()) != null) {
+  	        	Script += str;
+  	        }
+  	        in.close();
+			
+			EditTextScript.setText(Script.toString());
+			
+
+			} catch (Exception ex) {
+				Toast toast = Toast.makeText(this, "Error while loading script file " + loadFileName, Toast.LENGTH_LONG);
+				toast.show();
+			}
+		}
+	}
 	
 	/** Database */
 	protected static class DatabaseHelper extends SQLiteOpenHelper {
@@ -57,6 +96,18 @@ public class GScriptAdd extends Activity {
 
 		});
 		
+		Button btnLoadFile = (Button) findViewById(R.id.ButtonAddLoad);
+		btnLoadFile.setOnClickListener(
+				new OnClickListener()
+				{
+					public void onClick(View v) {
+		                Intent i = new Intent(GScriptAdd.this, GScriptLoad.class);
+		                startActivityForResult(i, ACTIVITY_LOAD_FILE);
+					}
+				}
+		);
+		
+		
 		Button btnSave = (Button) findViewById(R.id.ButtonAddSave);
 		btnSave.setOnClickListener(
 
@@ -90,7 +141,7 @@ public class GScriptAdd extends Activity {
 					}
 				}
 		);
-
+		
 	}
 
 }
